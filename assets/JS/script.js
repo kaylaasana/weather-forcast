798e80e801687a1c6fe7979376f054c81cf51bdb
kayla <you@example.com>
2023-07-11 20:39:55 -0700

    added HTML layout

---
+++ assets/JS/script.js
@@ -0,0 +1,45 @@
+// global variables, what needs to be stored and referenced in multiple functions
+    // request URLs with specific variable names
+    // store API keys
+    // element selectors
+
+// functions
+    // recent searches - check localStorage
+        // function that displays data on page
+    // search function --can be broken up into multiple functions if needed
+    
+    // function for storing search
+        // update/call recent searches function
+        // 
+
+    // function search(variable){
+        // take in input
+        // call function that saves recent searches
+    // }
+    // function coords(variable) {
+        // fetch request for coords with API
+        // .then - convert to JSON format
+        // .then - receive and extract coord data
+        // call weather function
+    // }
+    // function weather(coords) {
+        // use coords from first fetch to fetch weather data from API
+        // convert to JSON format
+        // extract temp, wind and humidity +5 day forecast from object data
+        // call display data function
+// }
+    // function display data(multiple variables, variable, variable, etc) {
+        // display data to page dependant on HTML
+// }
+    
+
+// event listener for search button ("click", function(){
+// variable for search input
+// call function - search(variable)
+// })
+
+// event listener for recent searches ("click", function(event){
+// console.log(event.target)
+// create variable for search term (text on button)
+// call search function
+// })
